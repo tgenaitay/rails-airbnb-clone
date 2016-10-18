b40f96ba4d59998b4e7d4a600ab569ce8bd78fba
thibault genaitay <tgenaitay@gmail.com>
2016-10-18 18:19:51 +0200

    fixed asset

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( fruity/MP4/Fruity.mp4 fruity/OGV/Fruity.ogv )
+Rails.application.config.assets.precompile += %w( Fruity/MP4/Fruity.mp4 Fruity/OGV/Fruity.ogv )
