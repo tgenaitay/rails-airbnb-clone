ea9aa9e5dee392f822696d3fe7dfe1e365f03d59
damien vincent <vincent.damien@gmail.com>
2016-10-18 16:24:59 +0200

    improve FB registration when no email

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      redirect_to new_user_registration_url, alert: 'Unable to get your email address from Facebook, please use regular registration'
     end
   end
 end
