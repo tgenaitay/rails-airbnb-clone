e81dc02905cb2f329890b1cf0d408d2118b8e7c0
thibault genaitay <tgenaitay@gmail.com>
2016-10-19 15:53:24 +0200

    fixed homepage and navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,10 @@ class PagesController < ApplicationController
     @user = User.find(params[:id])
     redirect_to :back, alert: 'FORBIDDEN, you can not view profile for non-chef user!' unless @user.is_chef?
   end
+
+  def home
+    @menus = Menu.all
+  end
 end
 
 
