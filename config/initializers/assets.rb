dc2477e06b729c602565fe361f889ab7f0cbbaad
damien vincent <vincent.damien@gmail.com>
2016-10-19 17:05:31 +0200

    fux (again) video pb on Heroku, try link to /public/videos instaed of /assets/videos

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( Fruity/MP4/Fruity.mp4 Fruity/OGV/Fruity.ogv )
-Rails.application.config.assets.paths << "#{Rails.root}/app/assets/video"
+# Rails.application.config.assets.precompile += %w( Fruity/MP4/Fruity.mp4 Fruity/OGV/Fruity.ogv )
+# Rails.application.config.assets.paths << "#{Rails.root}/app/assets/video"
