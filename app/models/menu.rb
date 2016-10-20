520069bce9e0bba85a5a847053444a30b22873c9
damien vincent <vincent.damien@gmail.com>
2016-10-20 16:07:30 +0200

    oders ok, but in separate view

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,23 @@
 class Menu < ApplicationRecord
   belongs_to :user
+  has_many :orders
   validates :name, :description, :price, :portions, :availability, presence: true
   validates_inclusion_of :category, :in => ["lunch", "dinner"], :allow_nil => false
   has_attachment :photo
+
+  before_save :check_stock
+
+  def ordered_quantity()
+    orders.inject(0){|sum,x| sum + (x.approved == false ? 0 : x.portions) }
+  end
+
+  def check_stock
+    qtt = self.ordered_quantity()
+    if qtt > self.portions
+      errors[:base] <<  "#{qtt} portion#{qtt>1 ? 's' : ''} are already booked, you can not have less portions available"
+      throw :abort
+    end
+
+  end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,23 @@ require 'date'
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :menu
+  validates :portions, numericality: { only_integer: true, greater_than: 0 }
+
+  before_save :check_stock
 
   def buddy_date
     self.date.strftime("%e %B, %Y")
   end
+
+  private
+
+  def check_stock
+    left = self.menu.portions - self.menu.ordered_quantity()
+    if (self.portions) > left
+      errors[:portions] <<  "You can not order more than #{left} portion#{left>1 ? 's' : ''}"
+      throw :abort
+    end
+
+  end
+
 end
