10cab0ab233873700a9685e9feaf110fbe09f93a
damien vincent <vincent.damien@gmail.com>
2016-10-20 10:53:18 +0200

    /chef root adaptation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [:chef, :home]
-  def chef
+  def landingchef
     @user = User.find(params[:id])
     redirect_to :back, alert: 'FORBIDDEN, you can not view profile for non-chef user!' unless @user.is_chef?
   end
