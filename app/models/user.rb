14989aec85246a2ef1a2b82dba5f86de1d5cca63
thibault genaitay <tgenaitay@gmail.com>
2016-10-21 14:59:48 +0200

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
 
   has_attachment :photo
 
+  geocoded_by :address
+
   def self.find_for_facebook_oauth(auth)
     user_params = auth.to_h.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
